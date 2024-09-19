30b7d2a149ddf2883027b448d6b050bf11f0520a
Pr0mise <kubo.setnicky@gmail.com>
2024-09-19 21:58:04 +0200

    feat(index);make content scrollable, add effets

--- js/index.js
+++ js/index.js
@@ -5,9 +5,30 @@ document.querySelector('.burger-menu').addEventListener('click', () => {
 
 document.querySelector('#daniela>img').addEventListener('click', () => {
     document.querySelector('#daniela-detail').classList.toggle('open');
-    document.querySelector('.detail-overlay').classList.toggle('open');
+});
+
+document.querySelector('#jakub>img').addEventListener('click', () => {
+    document.querySelector('#jakub-detail').classList.toggle('open');
+});
+
+document.querySelector('#dalibor>img').addEventListener('click', () => {
+    document.querySelector('#dalibor-detail').classList.toggle('open');
+});
+
+document.querySelector('#alex>img').addEventListener('click', () => {
+    document.querySelector('#alex-detail').classList.toggle('open');
+});
+
+document.querySelector('#jozef>img').addEventListener('click', () => {
+    document.querySelector('#jozef-detail').classList.toggle('open');
+});
+
+document.querySelector('#filip>img').addEventListener('click', () => {
+    document.querySelector('#filip-detail').classList.toggle('open');
 });
 
 function closeOverlay() {
-    document.querySelector('.team-card-detail').classList.remove('open');
+    for(e of document.getElementsByClassName('team-card-detail')) {
+        e.classList.remove('open');
+    }
 }
