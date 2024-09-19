80a45299cc5ed1a3ab80f7e89314757a14ba5cc6
Pr0mise <kubo.setnicky@gmail.com>
2024-09-19 20:48:40 +0200

    feat(index):basic layout and design

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
-function getWindow() {
-    return window;
+document.querySelector('.burger-menu').addEventListener('click', () => {
+    document.querySelector('.burger-menu').classList.toggle('open');
+    document.querySelector('.mobile-menu').classList.toggle('open');
+});
+
+document.querySelector('#daniela>img').addEventListener('click', () => {
+    document.querySelector('#daniela-detail').classList.toggle('open');
+    document.querySelector('.detail-overlay').classList.toggle('open');
+});
+
+function closeOverlay() {
+    document.querySelector('.team-card-detail').classList.remove('open');
 }
